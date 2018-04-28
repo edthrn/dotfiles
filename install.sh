ae8b986fbecebddd552022ac619f4ea7f1fb19a7
Edouard Theron <ed@edtheron.me>
2018-04-28 17:37:05 -0500

    Make directory a dotfile itself

--- install.sh
+++ install.sh
@@ -1,16 +1,17 @@
 cd ~/
-ln -s dotfiles/git/.gitconfig ~/
-ln -s dotfiles/git/.gitignore_global ~/
+mv dotfiles .dotfiles
+ln -s .dotfiles/git/.gitconfig ~/
+ln -s .dotfiles/git/.gitignore_global ~/
 echo ".gitconfig copied!"
-ln -s dotfiles/bash/.bashrc ~/
+ln -s .dotfiles/bash/.bashrc ~/
 echo ".bashrc copied!"
-ln -s dotfiles/vim/.vim ~/
-ln -s dotfiles/vim/.vimrc ~/
+ln -s .dotfiles/vim/.vim ~/
+ln -s .dotfiles/vim/.vimrc ~/
 echo "Vim conf-files copied!"
-ln -s dotfiles/tmux/.tmux-conf ~/
+ln -s .dotfiles/tmux/.tmux.conf ~/
 echo ".tmux-conf copied!"
-ln -s dotfiles/zsh/.zshrc ~/
+ln -s .dotfiles/zsh/.zshrc ~/
 echo ".zshrc copied!"
-ln -s dotfiles/zsh/edouard-custom.zsh-theme ~/.oh-my-zsh/themes/
-ln -s dotfiles/zsh/edouard-root.zsh-theme ~/.oh-my-zsh/themes/
+ln -s .dotfiles/zsh/edouard-custom.zsh-theme ~/.oh-my-zsh/themes/
+ln -s .dotfiles/zsh/edouard-root.zsh-theme ~/.oh-my-zsh/themes/
 echo "zsh-themes copied!"
